ffb0bfa46b5a2f245be7e192640f3032c628c4dc
Ian Flurkey <ianflurkey@gmail.com>
2024-06-06 18:17:45 -0400

    final commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 15, "F", "brown", "warm", 0);
+INSERT INTO bears VALUES (2, "Rowdy", 13, "M", "black", "mean", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 3, "F", "brown", "nice", 1);
+INSERT INTO bears VALUES (4, "Seargent Brown", 20, "M", "brown", "stoic", 0);
+INSERT INTO bears VALUES (5, "Melissa", 9, "F", "black", "loud", 0);
+INSERT INTO bears VALUES (6, "Grinch", 1, "M", "white", "compulsive", 1);
+INSERT INTO bears VALUES (7, "Wendy", 4, "F", "blue", "flighty", 1);
+INSERT INTO bears VALUES (8, null, 1, "F", "red", "angry", 1);
